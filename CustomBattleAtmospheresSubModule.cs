54927570d0ac11ab4f8f45fffe0086df62d9ac6a
poheniks <66043010+poheniks@users.noreply.github.com>
2022-04-20 17:22:58 -0500

    Add files via upload

---
+++ CustomBattleAtmospheresSubModule.cs
@@ -0,0 +1,38 @@
+﻿using HarmonyLib;
+using TaleWorlds.MountAndBlade;
+using TaleWorlds.Core;
+using System.Reflection;
+using TaleWorlds.Library;
+
+namespace CustomBattleAtmospheres
+{
+    public class CustomBattleAtmospheresSubModule : MBSubModuleBase
+    {
+        protected override void OnSubModuleLoad()
+        {
+            base.OnSubModuleLoad();
+            
+            Harmony harmony = new Harmony("CustomBattleAtmospheres");
+            MethodInfo o = typeof(BannerlordMissions).GetMethod("CreateAtmosphereInfoForMission", BindingFlags.NonPublic | BindingFlags.Static);
+            MethodInfo p = typeof(PatchCustomBattleAtmosphere).GetMethod("Postfix", BindingFlags.NonPublic | BindingFlags.Static);
+            harmony.Patch(o, postfix: new HarmonyMethod(p));
+        }
+
+        protected override void OnBeforeInitialModuleScreenSetAsRoot()
+        {
+            base.OnBeforeInitialModuleScreenSetAsRoot();
+            InformationManager.DisplayMessage(new InformationMessage("Custom Battle Atmospheres Loaded"));
+        }
+    }
+
+    [HarmonyPatch()]
+    class PatchCustomBattleAtmosphere
+    {
+        static void Postfix(ref AtmosphereInfo __result)
+        {
+            AtmosphereInfo atmosphere = new AtmosphereInfo();
+            atmosphere.AtmosphereName = "";
+            __result = atmosphere;
+        }
+    }
+}
